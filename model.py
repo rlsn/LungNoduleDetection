78511e1f25a380e4126870e7663ba0afdc3ffdc9
rlsn <yumo1996@gmail.com>
2024-04-11 23:40:37 +0900

    added 3d cnn backbone

--- model.py
+++ model.py
@@ -9,6 +9,89 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, downsample=None):
+        super().__init__()
+        self.conv1 = nn.Conv3d(
+            in_channels, 
+            out_channels, 
+            kernel_size=[3,3,3], 
+            stride=stride, 
+            padding=1,
+            bias=False
+        )
+        self.downsample = downsample
+        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv3d(
+            out_channels, 
+            out_channels, 
+            kernel_size=[3,3,3], 
+            padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm3d(out_channels)
+        
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+        return  out
+
+class CNNFeatureExtractor(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        patch_size = config.patch_size
+        image_size = config.image_size
+        self.in_channels = 128
+        
+        self.conv1 = nn.Conv3d(config.num_channels, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm3d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(128, 2)
+        self.layer2 = self._make_layer(256, 2, stride=2)
+        self.layer3 = self._make_layer(512, 2, stride=2)
+
+        # self.avgpool = nn.AdaptiveAvgPool3d([3, 8, 8])
+
+    def _make_layer(self, num_channels, num_layers, stride = 1):
+        downsample = None
+        if stride != 1:
+            downsample = nn.Sequential(
+                nn.Conv3d(self.in_channels, num_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm3d(num_channels),
+            )
+
+        layers = []
+        layers.append(ResBlock(self.in_channels, num_channels, stride, downsample))
+        self.in_channels = num_channels
+        for _ in range(1, num_layers):
+            layers.append(ResBlock(self.in_channels, num_channels, 1))
+
+        return nn.Sequential(*layers)
+
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        # x = self.avgpool(x)
+        x = x.flatten(2).transpose(1,2)
+        return x
+
 class Vit3DEmbeddings(nn.Module):
     def __init__(self, config):
         super().__init__()
